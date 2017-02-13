e1ee0c3b4659c7d57f971cadd9bc5c5da26dc677
brandon <town0082@algonquinlive.com>
2017-02-13 09:26:01 -0500

    commit 3

--- app/src/main/java/ac/phantom/SearchActivity.java
+++ app/src/main/java/ac/phantom/SearchActivity.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchActivity extends Activity {
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    ArrayAdapter<CharSequence> adapter
+            = ArrayAdapter.createFromResource(this,R.array.allergies,android.R.layout.simple_spinner_item);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
     public void searchOnClick(View view) {
