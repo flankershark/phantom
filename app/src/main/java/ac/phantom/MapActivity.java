5b38327c5a375eb524d21f3312257f0838b3a5c9
loliiiiipop <nun8866@gmail.com>
2017-03-13 01:52:19 -0400

    Show Current location

--- app/src/main/java/ac/phantom/MapActivity.java
+++ app/src/main/java/ac/phantom/MapActivity.java
@@ -1,8 +1,13 @@
 package ac.phantom;
-
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
-
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -10,9 +15,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private GoogleMap mMap;
-
+    private static final double
+            SEATTLE_LAT = 47.60621,
+            SEATTLE_LNG =-122.33207;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +29,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (mapFrag != null)
             mapFrag.getMapAsync(this);
     }
+    public boolean servicesOK() {
 
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog =
+                    GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
 
     /**
      * Manipulates the map once available.
