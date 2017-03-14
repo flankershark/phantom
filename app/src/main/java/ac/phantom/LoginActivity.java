070f19191e3b47f2281a0e10552fdbac3aa40dcb
TejasviJani <tejasvi.jani3@gmail.com>
2017-03-14 13:44:28 -0400

    implement login

--- app/src/main/java/ac/phantom/LoginActivity.java
+++ app/src/main/java/ac/phantom/LoginActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ac.phantom.model.User;
 
 public class LoginActivity extends Activity {
 
@@ -11,10 +15,23 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
     }
 
     public void loginOnClick(View view) {
-        Intent intent = new Intent(this, SearchActivity.class);
-        super.startActivity(intent);
+        EditText email = (EditText) findViewById(R.id.email_id);
+        EditText passwd = (EditText) findViewById(R.id.textPassword);
+        User user = DummyDataSource.login(email.getText().toString(), passwd.getText().toString());
+
+        if (user != null) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            intent.putExtra(DummyDataSource.EXTRA_RESTRICT, user.restrictions.toString());
+            super.startActivity(intent);
+        } else {
+            Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
