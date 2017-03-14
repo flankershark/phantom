c9e8a2ffda76f7aba6838db27207317479367a0d
fshark <flankershark@gmail.com>
2017-03-14 16:39:32 -0400

    Add sample dishes for demo

--- app/src/main/java/ac/phantom/DummyDataSource.java
+++ app/src/main/java/ac/phantom/DummyDataSource.java
@@ -41,12 +41,19 @@ public class DummyDataSource {
         restaurants = new ArrayList<>();
         restaurants.add(new Restaurant(1, "McDonald's", "1850 Baseline Rd, Nepean, ON K2L 0C6", "+1 (613) 255-0000"));
         restaurants.add(new Restaurant(2, "Tim Hortons", "4060 Carling Ave, Kanata, ON K2K 1Y1", "+1 (613) 255-1111"));
+        restaurants.add(new Restaurant(2, "Pizza Pizza", "673 Somerset St W, Ottawa, ON K1R 6P3", "+1 (613) 737-1111"));
 
         // constructs a number of dishes
         dishes = new ArrayList<>();
+        dishes.add(new Dish(6, "National Pride", "Topped with classic pepperoni, fresh mushrooms, bacon crumble and mozzarella cheese. Made by a proudly Canadian company. Tip: Try with cheddar jalapeno dipping sauce.", R.mipmap.pizza, restaurants.get(2)));
         dishes.add(new Dish(1, "Egg McMuffin", "It's way healthy than you thought", R.mipmap.eggmcmuffin, restaurants.get(0)));
+        dishes.add(new Dish(9, "Triple Thick Milkshake®", "Thick, smooth and deliciously creamy soft serve, mixed with your choice of decadent chocolate, strawberry or vanilla syrup for an extra taste of yumminess.", R.mipmap.mcdrink, restaurants.get(0)));
+        dishes.add(new Dish(5, "Chicken with Attitude", "Topped with chipotle chicken, red onions, chipotle BBQ sauce and mozzarella cheese.", R.mipmap.pizza, restaurants.get(2)));
         dishes.add(new Dish(2, "Peanut Butter Cookie", "Eat healthy eat fresh", R.mipmap.timcookies, restaurants.get(1)));
         dishes.add(new Dish(3, "Filet O Fish", "Nothing could be better", R.mipmap.filetofish, restaurants.get(0)));
+        dishes.add(new Dish(4, "Hot & Spicy Chicken", "Made with our Buffalo blue cheese sauce, grilled chicken, red onions, fire roasted red peppers & mozzarella cheese.", R.mipmap.pizza, restaurants.get(2)));
+        dishes.add(new Dish(8, "McFlurry®", "Deliciousness starts with creamy vanilla soft serve swirled together with your favourite mix-ins. Choose your McFlurry® treat, from SKOR® to Oreo® to M&M’s® candies and enjoy the yummy taste.", R.mipmap.mcdrink, restaurants.get(0)));
+        dishes.add(new Dish(7, "Sweet Heat", "Mesquite chicken, pineapple, hot banana peppers and mozzarella cheese", R.mipmap.pizza, restaurants.get(2)));
         for (int i = 0; i < dishes.size(); ++i)
             dishes.get(i).restrictions.add(restrictions.get(i % restrictions.size()));
     }
