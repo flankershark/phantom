e51c9500dd6b8253981e3313c499d3c9f06f3f1d
loliiiiipop <nun8866@gmail.com>
2017-04-10 13:27:53 -0400

    Current location and Destination

--- app/src/main/java/ac/phantom/MapActivity.java
+++ app/src/main/java/ac/phantom/MapActivity.java
@@ -1,29 +1,32 @@
-package ac.phantom;
-import android.app.Dialog;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+        package ac.phantom;
+        import android.app.Dialog;
+        import android.graphics.Color;
+        import android.os.Bundle;
+        import android.widget.Toast;
+        import android.support.v7.app.AppCompatActivity;
+        import com.google.android.gms.common.GooglePlayServicesUtil;
+        import com.google.android.gms.common.ConnectionResult;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.Marker;
+        import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.maps.model.Polyline;
+        import com.google.android.gms.maps.model.PolylineOptions;
+
+
+
+        public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private GoogleMap mMap;
     public static final double
             AC_LAT = 45.352386,
             AC_LNG =-75.754869;
-
+            int valueAddress=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,25 +63,56 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
+
     @Override
     public void onMapReady(GoogleMap m) {
         mMap = m;
-        LatLng CurrentLocation = new LatLng(AC_LAT , AC_LNG);
-            mMap
+        LatLng CurrentLocation = new LatLng(AC_LAT, AC_LNG);
+        mMap
                 .addMarker(new MarkerOptions()
-                .position(CurrentLocation).title("Algonquin College - Current location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CurrentLocation,10));
-        LatLng  RestaurantLocation = new LatLng(45.415313, -75.7560937);
-        mMap.addMarker(new MarkerOptions()
-                .position(RestaurantLocation)
-                .title(" McDonald`s - locate at 1380 College Square,Ottawa,On")
-                .snippet("2 min (550 m),via College Ave.Fastest route"));
+                        .position(CurrentLocation).title("Algonquin College - Current location"));
 
-        Polyline line = mMap.addPolyline(new PolylineOptions()
-                .add(new LatLng(AC_LAT , AC_LNG), new LatLng(45.415313,-75.7560937 ))
-                .width(5)
-                .color(Color.RED));
 
+        String addr = super.getIntent().getStringExtra(DummyDataSource.EXTRA_ADDR);
+        if (addr.equals("1850 Baseline Rd, Nepean, ON K2L 0C6")) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CurrentLocation, 15));
+            LatLng RestaurantLocation = new LatLng(45.354205, -75.756229);
+            mMap.addMarker(new MarkerOptions()
+                    .position(RestaurantLocation)
+                    .title(" McDonald`s - locate at 1380 College Square,Ottawa,On")
+                    .snippet("2 min (550 m),via College Ave.Fastest route"));
+
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .add(new LatLng(AC_LAT, AC_LNG), new LatLng(45.354205, -75.756229))
+                    .width(5)
+                    .color(Color.RED));
+
+        }else if(addr.equals("4060 Carling Ave, Kanata, ON K2K 1Y1")){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CurrentLocation, 10));
+            LatLng RestaurantLocation = new LatLng(45.341423, -75.908811);
+            mMap.addMarker(new MarkerOptions()
+                    .position(RestaurantLocation)
+                    .title(" Tim Hortons, 4060 Carling Ave, Kanata, ON K2K 1Y1")
+                    .snippet("15 min (15.3 km),via Trans-Canada Hwy/ON-417 W"));
+
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .add(new LatLng(AC_LAT, AC_LNG), new LatLng(45.341423, -75.908811))
+                    .width(5)
+                    .color(Color.RED));
+        }else{
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CurrentLocation, 15));
+            LatLng RestaurantLocation = new LatLng(45.354205, -75.756229);
+            mMap.addMarker(new MarkerOptions()
+                    .position(RestaurantLocation)
+                    .title("Pizza Pizza, 673 Somerset St W, Ottawa, ON K1R 6P3")
+                    .snippet("2 min (550 m),via College Ave.Fastest route"));
+
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .add(new LatLng(AC_LAT, AC_LNG), new LatLng(45.354205, -75.756229))
+                    .width(5)
+                    .color(Color.RED));
+
+        }
     }
 
 
